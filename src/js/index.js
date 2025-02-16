87ddca177c44a0775c14c0c3fc40b14a040196a3
Aline-Aguiar <aline.aglilly@gmail.com>
2025-02-16 18:20:09 -0300

    codificando cores do btn-saiba-mais em index.js e style.css

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 const imagens = document.querySelectorAll('.imagem-carrossel')
 const setaVoltar = document.getElementById('btn-voltar')
 const setaAvancar = document.getElementById('btn-avancar')
+const btnSaibaMais = document.getElementById('btn-saiba-mais')
 let imagemAtual = 0
 
 function esconderImagens() {
@@ -21,6 +22,35 @@ function habilitarSeta(seta){
     seta.classList.remove('desabilitar')
 }
 
+function corBotao(imagem){
+    switch (imagem){
+        case 0:
+            btnSaibaMais.classList.remove('color4');
+            btnSaibaMais.classList.remove('color3');
+            btnSaibaMais.classList.remove('color2');
+            btnSaibaMais.classList.add('color1');
+            break;
+        case 1:
+            btnSaibaMais.classList.remove('color4');
+            btnSaibaMais.classList.remove('color3');
+            btnSaibaMais.classList.remove('color1');
+            btnSaibaMais.classList.add('color2');
+            break;
+        case 2:
+            btnSaibaMais.classList.remove('color4');
+            btnSaibaMais.classList.remove('color2');
+            btnSaibaMais.classList.remove('color1');
+            btnSaibaMais.classList.add('color3');
+            break;
+        case 3:
+            btnSaibaMais.classList.remove('color3');
+            btnSaibaMais.classList.add('color4');
+            break;
+        default:
+            btnSaibaMais.classList.add('color1');
+    }
+}
+
 setaAvancar.addEventListener('click', function () {
     if(imagemAtual !== imagens.length - 1) {
         habilitarSeta(setaVoltar)
@@ -30,6 +60,7 @@ setaAvancar.addEventListener('click', function () {
 
     esconderImagens()
     mostrarImagem()
+    corBotao(imagemAtual)
 })
 
 setaVoltar.addEventListener('click', function () {
@@ -42,4 +73,5 @@ setaVoltar.addEventListener('click', function () {
 
     esconderImagens()
     mostrarImagem()
+    corBotao(imagemAtual)
 })
